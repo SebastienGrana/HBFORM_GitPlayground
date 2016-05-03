00a1327868a44f7b704492c6f7c7a4f3593b41c1
Sebastien <granarolo.sebastien@gmail.com>
2016-05-03 10:29:42 +0200

    TroisiemeGitMain

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 
 public class Main {
-
+	
+	public static void blabla(){
+		System.out.println("blablablabla");
+	}
+	
+	public static void compte(){
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+		}
+	}
+	
+	public static void pomme(){
+		System.out.println("le meilleur pomme est la golden");
+	}
+	
+	
+	
 }
